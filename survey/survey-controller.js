ac09a5ef49870e9cf7c359751f27087f2664e213
Jitender Singh <jitender.iitd11@gmail.com>
2018-10-07 23:02:45 +0530

    pushing branch for dev

--- survey/survey-controller.js
+++ survey/survey-controller.js
@@ -2,26 +2,29 @@
  * bcbsnc.med.survey
  *
  * @category Controllers
- * @class MyInterestsCtrl
+ * @class SurveyCtrl
  * @constructor
  * @parameter  {Object}                $scope                AngularJS scope service.
  * @parameter  {Object}                $rootScope            AngularJS rootScope service.
- * @parameter  {Object}                $timeout              AngularJS timeout service.
- * @parameter  {ScreenSizeFactory}      ScreenSizeFactory      A [ScreenSizeFactory](#/class/screensizefactory) instance.
  */
 angular.module('bcbsnc.med.survey', ['ui.router'])
 	.controller('SurveyCtrl',
 	//@ngInject
-	function ($scope, $rootScope, $timeout, ScreenSizeFactory) {
+	function ($scope, $rootScope) {
 
+		$scope.isMemberHasTeleHealth = bcbsnc.dashboard.features.telehealth;
+		$scope.isMemberHasHealthLineBlue = bcbsnc.dashboard.features.healthLineBlue;
+		$scope.isMemberRewardble = bcbsnc.thisMember.blueRewards.available;
+		
 		$scope.firstQPage = true;
 		$scope.secondQPage = false;
 		$scope.thirdQPage = false;
 		$scope.fourthQPage = false;
-		$scope.isMemberHasTeleHealth = false;
-		$scope.isMemberHasHealthLineBlue = false;
-		$scope.isMemberHasReward = false;
+		$scope.teleHealthPage = false;
+		$scope.healthLineBluePage = false;
+		$scope.rewardPage = false;
 		$scope.surveyConfirmation = false;
+		$scope.printCustomizedGuide = false;
 		$scope.hideSubmitBtn = false;
 		
 		/*
@@ -40,8 +43,8 @@ angular.module('bcbsnc.med.survey', ['ui.router'])
 		};
 
         $scope.submitFirstAnswer = function(e) {
-            console.log('event- ', $scope.firstAnswers, bcbsnc);
-			e.preventDefault();
+			console.log(bcbsnc);
+            e.preventDefault();
 			if ($scope.firstAnswers.length === 3) {
 				$scope.showAnsweredCorrectly = true;
 			} else {
@@ -60,13 +63,11 @@ angular.module('bcbsnc.med.survey', ['ui.router'])
 		/*
 		 * 2nd Question
 		 */
-
 		$scope.secondAnsweredYes = false;
 		$scope.secondAnsweredNo = false;
 		$scope.secondAnswer = '';
 		$scope.selectSecondAnswer = function(e) {
 			$scope.secondAnswer = e.target.value;
-			console.log('event- ', $scope.secondAnswer);
 			if ($scope.hideSubmitBtn) {
 				$scope.secondAnsweredYes = $scope.secondAnswer === 'Yes';
 				$scope.secondAnsweredNo = $scope.secondAnswer === 'No';
@@ -74,8 +75,7 @@ angular.module('bcbsnc.med.survey', ['ui.router'])
 		}
 
 		$scope.submitSecondAnswer = function(e) {
-            console.log('event- ', $scope.secondAnswer);
-			e.preventDefault();
+            e.preventDefault();
 			if ($scope.secondAnswer === "") {
 				return true;
 			}
@@ -89,7 +89,6 @@ angular.module('bcbsnc.med.survey', ['ui.router'])
 		};
 
 		$scope.next3 = function (e) {
-			console.log('next 3:');
 			e.preventDefault();
 			$scope.hideSubmitBtn = false;
 			$scope.secondQPage = false;
@@ -112,8 +111,7 @@ angular.module('bcbsnc.med.survey', ['ui.router'])
 		};
 
         $scope.submitThirdAnswer = function(e) {
-            console.log('event- ', $scope.thirdAnswers);
-			e.preventDefault();
+            e.preventDefault();
 			if ($scope.thirdAnswers.length === 4) {
 				$scope.showThirdAnsweredCorrectly = true;
 			} else {
@@ -132,12 +130,10 @@ angular.module('bcbsnc.med.survey', ['ui.router'])
 		/*
 		 * 4th Question
 		 */
-
 		$scope.fourthAnsweredYes = false;
 		$scope.fourthAnsweredNo = false;
 		$scope.fourthAnswer = '';
 		$scope.selectFourthAnswer = function(e) {
-			console.log('event- ', e.target.value);
 			$scope.fourthAnswer = e.target.value;
 			if ($scope.hideSubmitBtn) {
 				$scope.fourthAnsweredYes = $scope.fourthAnswer === 'Yes';
@@ -146,8 +142,7 @@ angular.module('bcbsnc.med.survey', ['ui.router'])
 		}
 
 		$scope.submitFourthAnswer = function(e) {
-            console.log('event- ', $scope.fourthAnswer);
-			e.preventDefault();
+            e.preventDefault();
 			if ($scope.fourthAnswer === "") {
 				return true;
 			}
@@ -161,11 +156,18 @@ angular.module('bcbsnc.med.survey', ['ui.router'])
 		};
 
 		$scope.next5 = function (e) {
-			console.log('next 3:');
 			e.preventDefault();
 			$scope.hideSubmitBtn = false;
 			$scope.fourthQPage = false;
-			$scope.isMemberHasTeleHealth = true;	
+			if ($scope.isMemberHasTeleHealth) {
+				$scope.teleHealthPage = true;
+				return true;
+			}
+			if ($scope.isMemberHasHealthLineBlue) {
+				$scope.healthLineBluePage = true;
+				return true;
+			}
+			$scope.rewardPage = true;		
 		}
 
 		/*
@@ -184,8 +186,7 @@ angular.module('bcbsnc.med.survey', ['ui.router'])
 		};
 
         $scope.submitTeleHealthAnswers = function(e) {
-            console.log('event- ', $scope.teleHealthAnswers);
-			e.preventDefault();
+            e.preventDefault();
 			if ($scope.teleHealthAnswers.length === 0) {
 				return true;
 			}
@@ -200,8 +201,12 @@ angular.module('bcbsnc.med.survey', ['ui.router'])
 		$scope.next6 = function (e) {
 			e.preventDefault();
 			$scope.hideSubmitBtn = false;
-			$scope.isMemberHasTeleHealth = false;
-			$scope.isMemberHasHealthLineBlue = true;
+			$scope.teleHealthPage = false;
+			if ($scope.isMemberHasHealthLineBlue) {
+				$scope.healthLineBluePage = true;
+				return true;
+			}
+			$scope.rewardPage = true;
 		}
 
 		/*
@@ -220,8 +225,7 @@ angular.module('bcbsnc.med.survey', ['ui.router'])
 		};
 
         $scope.submitHealthLineBlueAnswers = function(e) {
-            console.log('event- ', $scope.healthLineBlueAnswers);
-			e.preventDefault();
+            e.preventDefault();
 			if ($scope.healthLineBlueAnswers.length === 4) {
 				$scope.showHealthLineBlueAnsweredCorrectly = true;
 			} else {
@@ -231,67 +235,28 @@ angular.module('bcbsnc.med.survey', ['ui.router'])
 		};
 		
 		$scope.next7 = function (e) {
-			console.log($scope);
 			e.preventDefault();
 			$scope.hideSubmitBtn = false;
-			$scope.isMemberHasHealthLineBlue = false;
-			$scope.isMemberHasReward = true;
+			$scope.healthLineBluePage = false;
+			$scope.rewardPage = true;
 		}
 
 		/*
 		 * Rewards
 		 */
-
 		$scope.onInputChange = function(e) {
-			console.log(e);
-			$scope[e.target.name] = e.target.value
+			$scope[e.target.name] = e.target.value;
 		}
 
 		$scope.printCustomizedGuide = function() {
 			console.log('printing');
 			$scope.surveyConfirmation = true;
-			window.print();
-			// var documentId = 'pdfDocument';
-			// var doc = document.getElementById(documentId);
-
-			// //Wait until pdf is ready to print    
-			// if (typeof doc.print === 'undefined') {    
-			// 	setTimeout(function(){printCustomizedGuide(documentId);}, 1000);
-			// } else {
-			// 	doc.print();
-			// }
+			
+			var documentId = 'pdfDocument';
+			var doc = document.getElementById(documentId);
+			console.log('doc- ', doc);
+			doc.contentWindow.focus()
+			doc.contentWindow.print();
 		}
-
-		
-		$scope.draggable = Modernizr.touch ? "" : "true";
-
-		/*
-		 * listen for reload event from mobile interests drawer / window-resize
-		 */
-		var u = $rootScope.$on('Interests:reload', function (event, notifyAdobe) {
-			getMyInterestsTiles(notifyAdobe);
-
-			if(ScreenSizeFactory.screenSize() == 'desktop'){
-				$scope.arrangeMode = false;
-				$scope.draggable = Modernizr.touch ? "" : "true";
-			}
-		});
-		$scope.$on('$destroy', u);
-
-		/*
-		 * Listen for "Arrange" mode via tile-drawer-controller.js
-		 */
-		$scope.arrangeMode = false;
-		var e = $rootScope.$on('Interests:arrangeMode', function (event, enable) {
-			$scope.draggable = enable ? "true": "";
-			$scope.arrangeMode = $scope.draggable;
-
-			getMyInterestsTiles(false);
-		});
-		$scope.$on('$destroy', e);
-
-		$scope.getTileClass = function(style, color){
-			return style + ' ' + color + ($scope.arrangeMode ? ' arrange-mode' : '');
-		};
 	}
 );
